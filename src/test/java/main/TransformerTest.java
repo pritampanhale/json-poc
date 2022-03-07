59dd94877fe0dff104c3e1306fe050b734bb633a
1038472 <1038472@INBLRLT4609.quest-global.com>
2022-03-07 18:31:31 +0530

    # WARNING: head commit changed in the meantime
    [Balaji Kuruba] - Added Junit dependencies and sample test case

---
+++ src/test/java/main/TransformerTest.java
@@ -0,0 +1,12 @@
+package main;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TransformerTest {
+	@Test
+    public void test() {
+		assertTrue(true);
+    }
+}
